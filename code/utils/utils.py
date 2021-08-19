dd38b3ec8fc88f158b6d9e6bdf59ed58834ea23e
qkicen <dyf@my.swjtu.edu.cn>
2021-08-19 15:26:59 +0800

    Create utils.py

---
+++ code/utils/utils.py
@@ -0,0 +1,37 @@
+import os
+import json
+import logging
+
+import numpy as np
+
+import torch
+
+def create_logger(save_path='', file_type='train', level='debug'):
+
+    if level == 'debug':
+        _level = logging.DEBUG
+    elif level == 'info':
+        _level = logging.INFO
+
+    logger = logging.getLogger()
+    logger.setLevel(_level)
+
+    cs = logging.StreamHandler()
+    cs.setLevel(_level)
+    logger.addHandler(cs)
+
+    if save_path != '':
+        file_name = os.path.join(save_path, file_type + '_log.txt')
+        fh = logging.FileHandler(file_name, mode='w')
+        fh.setLevel(_level)
+
+        logger.addHandler(fh)
+
+    return logger
+
+def print_args(args, logger=None):
+    for k, v in vars(args).items():
+        if logger is not None:
+            logger.info('{:<16} : {}'.format(k, v))
+        else:
+            print('{:<16} : {}'.format(k, v))
