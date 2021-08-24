b7c88d9a0b47e579baca74a991921b3e2c4b9950
realcrane <realcrane@gmail.com>
2021-08-24 15:22:04 +0100

    update

--- code/classifiers/STGCN.py
+++ code/classifiers/STGCN.py
@@ -237,7 +237,6 @@ class STGCN(ActionClassifier):
                 pred = self.model(X)
                 loss = self.classLoss(pred, y)
 
-
                 # Backpropagation
                 self.optimiser.zero_grad()
                 loss.backward()
@@ -280,44 +279,44 @@ class STGCN(ActionClassifier):
             #print(f"epoch: {ep} GPU memory allocated after one epoch validation: {torch.cuda.memory_allocated(1)}")
 
 
-    def test(self):
-        self.model.eval()
-        misclassified = 0
-        results = np.empty(len(self.testloader.dataset.rlabels))
-        for v, (tx, ty) in enumerate(self.testloader):
-            pred = torch.argmax(self.model(tx), dim=1)
-            results[v*self.args.batchSize:(v+1)*self.args.batchSize] = pred.cpu()
-            diff = (pred - ty) != 0
-            misclassified += torch.sum(diff)
+    # def test(self):
+    #     self.model.eval()
+    #     misclassified = 0
+    #     results = np.empty(len(self.testloader.dataset.rlabels))
+    #     for v, (tx, ty) in enumerate(self.testloader):
+    #         pred = torch.argmax(self.model(tx), dim=1)
+    #         results[v*self.args.batchSize:(v+1)*self.args.batchSize] = pred.cpu()
+    #         diff = (pred - ty) != 0
+    #         misclassified += torch.sum(diff)
+    #
+    #     acc = 1 - misclassified / len(self.testloader.dataset)
+    #
+    #     print(f"accuracy: {acc:>4f}")
+    #     np.savetxt(self.retFolder + 'testRets.txt', results)
+    #     np.savetxt(self.retFolder + 'testGroundTruth.txt', self.testloader.dataset.rlabels)
+    #
+    #     return acc
 
-        acc = 1 - misclassified / len(self.testloader.dataset)
-
-        print(f"accuracy: {acc:>4f}")
-        np.savetxt(self.retFolder + 'testRets.txt', results)
-        np.savetxt(self.retFolder + 'testGroundTruth.txt', self.testloader.dataset.rlabels)
-
-        return acc
-
-    # this function is to collected all the testing samples that can be correctly collected
-    # by the pre-trained classifier, to make a dataset for adversarial attack
-    def collectCorrectPredictions(self):
-        self.model.eval()
-
-        # collect data from the test data
-        misclassified = 0
-        results = np.empty(len(self.testloader.dataset.rlabels))
-        for v, (tx, ty) in enumerate(self.testloader):
-            pred = torch.argmax(self.model(tx), dim=1)
-            diff = (pred - ty) == 0
-            results[v * self.args.batchSize:(v + 1) * self.args.batchSize] = diff.cpu()
-
-        adData = self.testloader.dataset.data[results.astype(bool)]
-        adLabels = self.testloader.dataset.rlabels[results.astype(bool)]
-
-        print(f"{len(adLabels)} out of {len(results)} motions are collected")
-
-        if not os.path.exists(self.retFolder):
-            os.mkdir(self.retFolder)
-        np.savez_compressed(self.retFolder+self.args.adTrainFile, clips=adData, classes=adLabels)
-
-        return len(adLabels)
+    # # this function is to collected all the testing samples that can be correctly collected
+    # # by the pre-trained classifier, to make a dataset for adversarial attack
+    # def collectCorrectPredictions(self):
+    #     self.model.eval()
+    #
+    #     # collect data from the test data
+    #     misclassified = 0
+    #     results = np.empty(len(self.testloader.dataset.rlabels))
+    #     for v, (tx, ty) in enumerate(self.testloader):
+    #         pred = torch.argmax(self.model(tx), dim=1)
+    #         diff = (pred - ty) == 0
+    #         results[v * self.args.batchSize:(v + 1) * self.args.batchSize] = diff.cpu()
+    #
+    #     adData = self.testloader.dataset.data[results.astype(bool)]
+    #     adLabels = self.testloader.dataset.rlabels[results.astype(bool)]
+    #
+    #     print(f"{len(adLabels)} out of {len(results)} motions are collected")
+    #
+    #     if not os.path.exists(self.retFolder):
+    #         os.mkdir(self.retFolder)
+    #     np.savez_compressed(self.retFolder+self.args.adTrainFile, clips=adData, classes=adLabels)
+    #
+    #     return len(adLabels)
